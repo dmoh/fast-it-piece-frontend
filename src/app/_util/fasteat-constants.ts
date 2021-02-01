c9878daf1c766a0c31b9474da00e2a6513555fd3
Elhade <elh-13@hotmail.fr>
2021-02-01 06:06:02 +0100

    Deliverer ok

---
+++ src/app/_util/fasteat-constants.ts
@@ -0,0 +1,10 @@
+export const status = {
+    WAITING_RESPONSE_BUSINESS: 1,
+    WAITING_TAKING_BY_DELIVER: 2,
+    ORDER_ROUTING: 3,
+    ORDER_CLOSED_WITH_SUCCESS: 4,
+    ORDER_REFUSED: 0,
+};
+export const order = {
+  SERVICE_CHARGE: 80
+};
